e1afa9e683a8dd7fb3384170a8e763ac8d1ea81b
Pablo Company Ramírez <p.c.ramirez5150@gmail.com>
2021-01-15 13:44:46 +0100

    Adds dynamic parameters to query

--- DapperExploration.WebApi/Controllers/DapperExplorationController.cs
+++ DapperExploration.WebApi/Controllers/DapperExplorationController.cs
@@ -1,4 +1,5 @@
-﻿using Dapper;
+﻿using System.Collections.Generic;
+using Dapper;
 using DapperExploration.WebApi.Models;
 using Microsoft.AspNetCore.Mvc;
 using Npgsql;
@@ -14,7 +15,7 @@ namespace DapperExploration.WebApi.Controllers
             "User ID=postgres;Password=postgres;Host=localhost;Port=5432;Database=personal_site_db;";
 
         [HttpGet]
-        public async Task<IActionResult> GetDapper()
+        public IEnumerable<JobExperience> GetDapper()
         {
             // Note:
             // NpgsqlConnection class is an administered type that connects with non-administered
@@ -24,13 +25,17 @@ namespace DapperExploration.WebApi.Controllers
             // Is the same than putting the code in a try block and then calling Dispose() method in the finally block
             // In fact, this is how the compiler traduces the using statement
 
-            
-            await using var sqlConnection = new NpgsqlConnection(SqlConnectionString);
+            using var sqlConnection = new NpgsqlConnection(SqlConnectionString);
             var sqlCommand = @"SELECT ""Id"", ""Company"", ""Description"", ""TechStack"", ""JobPeriod_End"", ""JobPeriod_Start""
-                                FROM public.""JobExperiences"";";
-            var jobExperiences = await sqlConnection.QueryAsync<JobExperience>(sqlCommand); 
+                                FROM public.""JobExperiences""";
 
-            return Ok(jobExperiences);
+            var dynamicParameters = new DynamicParameters();
+            sqlCommand += " WHERE \"Company\" = @company";
+            dynamicParameters.Add("Company", "Microsofta");
+
+            var jobExperiences = sqlConnection.Query<JobExperience>(sqlCommand, dynamicParameters); 
+
+            return jobExperiences;
         }
     }
 }
