c6c5f31aef2d3375a7ad5a765633749aaf51100e
Pablo Company Ramírez <p.c.ramirez5150@gmail.com>
2021-01-15 13:30:30 +0100

    Agregar archivos de proyecto.

---
+++ DapperExploration.WebApi/Controllers/DapperExplorationController.cs
@@ -0,0 +1,36 @@
+﻿using Dapper;
+using DapperExploration.WebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+using System.Threading.Tasks;
+
+namespace DapperExploration.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DapperExplorationController : ControllerBase
+    {
+        private const string SqlConnectionString =
+            "User ID=postgres;Password=postgres;Host=localhost;Port=5432;Database=personal_site_db;";
+
+        [HttpGet]
+        public async Task<IActionResult> GetDapper()
+        {
+            // Note:
+            // NpgsqlConnection class is an administered type that connects with non-administered
+            // using directive manages IDisposable objects and it ensures that Dispose() method is being called
+            // even if a exception is thrown in the execution
+
+            // Is the same than putting the code in a try block and then calling Dispose() method in the finally block
+            // In fact, this is how the compiler traduces the using statement
+
+            
+            await using var sqlConnection = new NpgsqlConnection(SqlConnectionString);
+            var sqlCommand = @"SELECT ""Id"", ""Company"", ""Description"", ""TechStack"", ""JobPeriod_End"", ""JobPeriod_Start""
+                                FROM public.""JobExperiences"";";
+            var jobExperiences = await sqlConnection.QueryAsync<JobExperience>(sqlCommand); 
+
+            return Ok(jobExperiences);
+        }
+    }
+}
---
+++ DapperExploration.WebApi/Models/JobExperience.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DapperExploration.WebApi.Models
+{
+    public class JobExperience
+    {
+        public string Id { get; set; }
+        public string Company { get; set; }
+        public string Description { get; set; }
+        public string[] TechStack { get; set; }
+        public DateTime JobPeriod_Start { get; set; }
+        public DateTime? JobPeriod_End { get; set; }
+    }
+}
